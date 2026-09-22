57974d129e0e8a379145f8f1411634587993a4ae
Izzis <al@izs.web.id>
2026-09-22 16:05:00 +0700

    Bump version to 1.1.3 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "id.web.izs.nettools"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.3"
     }
 
     buildTypes {
