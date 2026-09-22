2740b614dbf6f1484cafc38cb08b8d2803f96aa8
Izzis <al@izs.web.id>
2026-09-22 15:37:50 +0700

    Bump version to 1.1.2 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "id.web.izs.nettools"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
     }
 
     buildTypes {
